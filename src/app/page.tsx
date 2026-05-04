041466d2bc49faf4566595135140c9c5d083240d
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-05-04 16:38:58 +0330

    initial nextjs portfolio

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Hi, I’m Mahan 👋</h1>
+    </main>
+  );
+}
