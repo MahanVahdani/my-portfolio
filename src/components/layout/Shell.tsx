5f8b63c57e610a9e08963bac890b10e84469cb62
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-05-07 09:39:11 +0330

    feat: add responsive shell layout and global content spacing

--- src/components/layout/Shell.tsx
+++ src/components/layout/Shell.tsx
@@ -1,14 +1,30 @@
 import React from "react";
-import ThemeToggle from "@/lib/theme/ThemeToggle";
 
 const Shell = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <header className="flex justify-end p-4">
-        <ThemeToggle />
-      </header>
+    <div className="min-h-screen">
+      <div
+        className="
+          w-full
+          max-w-7xl
+          mx-auto
+          px-3
+          sm:px-5
+          lg:px-8
+          xl:px-10
+          "
+      >
+        <div className="flex gap-6">
+          {/* Left Sidebar */}
+          <aside className="hidden lg:block w-72 shrink-0">Sidebar</aside>
 
-      {children}
+          {/* Main Content */}
+          <main className="flex-1 py-12 md:py-20">{children}</main>
+
+          {/* Right Navigation */}
+          <aside className="hidden xl:block w-20 shrink-0">Nav</aside>
+        </div>
+      </div>
     </div>
   );
 };
