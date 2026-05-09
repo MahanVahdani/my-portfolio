ccb558dd18ab47a552e1662863fb679baa96ee0d
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-05-09 17:04:28 +0330

    git commit -m "fix(shell): correct main layout spacing for fixed sidebars"

--- src/components/layout/Shell.tsx
+++ src/components/layout/Shell.tsx
@@ -4,14 +4,26 @@ import SidebarProfile from "./SidebarProfile";
 
 const Shell = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen w-full">
+    <div className="min-h-screen w-full flex flex-col lg:block">
       {/* Left fixed area */}
       <aside
-        className="w-62 h-120 fixed left-6 top-1/2 
-        hidden -translate-y-1/2 lg:block xl:w-72"
+        className="
+          lg:fixed lg:left-6 lg:top-1/2 lg:-translate-y-1/2
+          w-full lg:w-72 px-4 lg:px-0 pt-6 lg:pt-0
+        "
       >
         <SidebarProfile />
       </aside>
+
+      {/* Main content */}
+      <main
+        className="
+        max-w-5xl px-4 lg:px-6 bg-amber-300
+        py-10 lg:py-20 lg:ml-77.5 lg:mr-22.5
+        "
+      >
+        {children}
+      </main>
 
       {/* Right fixed nav */}
       <aside
@@ -20,9 +32,6 @@ const Shell = ({ children }: { children: React.ReactNode }) => {
       >
         <SectionNavigator />
       </aside>
-
-      {/* Main content */}
-      {/* <main className="mx-auto max-w-5xl px-6 py-20">{children}</main> */}
     </div>
   );
 };
