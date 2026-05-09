5ce29abd471edb02ec98434c7bf0cb868fe20758
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-05-09 17:19:48 +0330

    git commit -m "fix(sidebar-profile): improve tablet and mobile responsive layout"

--- src/components/layout/SidebarProfile.tsx
+++ src/components/layout/SidebarProfile.tsx
@@ -7,33 +7,37 @@ import profile from "@/data/profile";
 const SidebarProfile = () => {
   return (
     <GlassCard className="w-70 h-full p-6 flex flex-col justify-evenly text-center">
-      {/* TOP */}
-      <div className="flex flex-col items-center gap-4">
-        {/* Avatar */}
-        <div className="h-24 w-24 xl:h-28 xl:w-28 overflow-hidden rounded-full bg-white flex items-center justify-center">
-          <Image
-            src={profile.avatar}
-            alt={profile.name}
-            width={96}
-            height={96}
-          />
-        </div>
+      {/* Avatar */}
+      <div
+        className="h-24 w-24 xl:h-28 xl:w-28 overflow-hidden rounded-full
+       bg-white flex self-center"
+      >
+        <Image
+          className="object-contain"
+          src={profile.avatar}
+          alt={profile.name}
+          width={100}
+          height={100}
+        />
+      </div>
 
+      {/* Middle Part */}
+      <div className="mt-2 flex flex-col justify-between md:flex-row lg:flex-col">
         {/* Name + Role */}
         <div className="space-y-1">
           <h2 className="text-xl font-semibold">{profile.name}</h2>
           <p className="text-sm text-muted">{profile.role}</p>
         </div>
-      </div>
 
-      {/* MIDDLE */}
-      <div className="flex flex-col items-center gap-3 mt-3">
-        <ProfileSocialLinks links={profile.socialLinks} />
-        <CopyEmailButton email={profile.email} />
+        {/* Social Media */}
+        <div className="flex flex-col items-center mt-3 md:-mt-1 lg:mt-3">
+          <ProfileSocialLinks links={profile.socialLinks} />
+          <CopyEmailButton email={profile.email} />
+        </div>
       </div>
 
       {/* BOTTOM */}
-      <div className="flex flex-col gap-4 mt-3">
+      <div className="flex flex-col gap-4 mt-6">
         <a
           href="#contact"
           className="inline-flex w-full items-center justify-center rounded-xl bg-primary px-5 py-3 text-sm font-medium text-black transition-all hover:scale-[1.02]"
