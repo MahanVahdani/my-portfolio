c3e13e8a59aed92933aa3cc20a1703a46f5cced7
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-05-10 13:40:02 +0330

    feat(hero): integrate HeroSectionButtons into HeroSection

---
+++ src/components/sections/hero/HeroSection.tsx
@@ -0,0 +1,22 @@
+import SectionBlock from "@ui/SectionBlock";
+import HeroActionButton from "@/components/sections/hero/HeroActionButton";
+
+const HeroSection = () => {
+  return (
+    // text lenght ***
+    <SectionBlock
+      id="hello"
+      mutedTitle="Hi, I’m Mahan Vahdani"
+      accentTitle="Senior Frontend Developer"
+      description="I build fast, clean, and scalable web experiences with Next.js and React. Focused on high-performance interfaces, thoughtful architecture, and delivering measurable business impact."
+      className="scroll-mt-24 py-20"
+    >
+      <div className="flex flex-wrap gap-3">
+        <HeroActionButton label="Download Resume" variant="primary" />
+        <HeroActionButton label="Contact Me" variant="outlined" />
+      </div>
+    </SectionBlock>
+  );
+};
+
+export default HeroSection;
