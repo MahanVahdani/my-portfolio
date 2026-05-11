23e84db71ac345ce5fd52569dd05180de9b21a48
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-05-11 14:35:19 +0330

    feat(skills): create reusable SkillCard component with glass UI and progress display

---
+++ src/components/sections/skill/SkillCard.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import GlassCard from "@/components/ui/GlassCard";
+import { cn } from "@/lib/utils";
+
+type SkillCardProps = {
+  name: string;
+  percentage: number;
+  logo: string;
+  className?: string;
+};
+
+const SkillCard = ({ name, percentage, logo, className }: SkillCardProps) => {
+  return (
+    <GlassCard
+      className={cn(
+        `
+      max-w-45
+      flex flex-col items-center justify-center
+      gap-4 p-5
+      text-center
+      transition-all duration-300
+      hover:-translate-y-1
+      hover:shadow-lg
+    `,
+        className,
+      )}
+    >
+      {/* Logo */}
+      <div
+        className="
+        h-18 w-18
+        rounded-2xl
+      bg-black/5 dark:bg-white/10
+        flex items-center justify-center
+    "
+      >
+        <Image
+          src={logo}
+          alt={name}
+          width={45}
+          height={45}
+          className="object-contain "
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        {/* Percentage */}
+        <div className="text-sm text-primary font-semibold ">{percentage}%</div>
+
+        {/* Name */}
+        <div className=" text-sm text-muted-foreground font-medium">{name}</div>
+      </div>
+    </GlassCard>
+  );
+};
+
+export default SkillCard;
