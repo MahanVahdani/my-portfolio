f8805f6b738b102c327b82d9f8eb108e515162d2
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-06-06 16:19:41 +0330

    move tesminoal section component from main folder.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import ExperienceSection from "@/components/sections/experience/ExperienceSectio
 import HeroSection from "@/components/sections/hero/HeroSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
 import SkillsSection from "@/components/sections/skill/SkillsSection";
-import TestimonialsSection from "@/components/sections/TestimonialsSection";
+import TestimonialsSection from "@/components/sections/Testimonial/TestimonialsSection";
 import PageView from "@/lib/analytics/PageView";
 
 const Home = () => {
