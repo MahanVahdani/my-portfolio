85bf66bdfbc92c3407ac32a8dddb9e02931cdcdc
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-06-09 13:17:26 +0330

    add primary data template for projects.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,56 @@
-export const projects = [
+export interface Project {
+  id: number;
+  name: string;
+  logo: string;
+  image: string;
+  description: string;
+  link: string;
+}
+
+export const portfolioProjects: Project[] = [
   {
-    title: "...",
-    image: "...",
-    description: "...",
-    link: "...",
+    id: 1,
+    name: "Aqaye Pardakht",
+    logo: "/images/companies/aqa.jpeg",
+    image: "/images/projects/AqaStatus.jpeg",
+    description:
+      "Built a full-stack dashboard using React, Next.js, and Prisma.",
+    link: "https://status.aqayepardakht.ir",
+  },
+  {
+    id: 1,
+    name: "Aqaye Pardakht",
+    logo: "/images/companies/aqa.jpeg",
+    image: "/images/projects/AqaStatus.jpeg",
+    description:
+      "Built a full-stack dashboard using React, Next.js, and Prisma.",
+    link: "https://status.aqayepardakht.ir",
+  },
+  {
+    id: 2,
+    name: "Aqaye Pardakht",
+    logo: "/images/companies/aqa.jpeg",
+    image: "/images/projects/AqaStatus.jpeg",
+    description:
+      "Built a full-stack dashboard using React, Next.js, and Prisma.",
+    link: "https://status.aqayepardakht.ir",
+  },
+  {
+    id: 3,
+    name: "Aqaye Pardakht",
+    logo: "/images/companies/aqa.jpeg",
+    image: "/images/projects/AqaStatus.jpeg",
+    description:
+      "Built a full-stack dashboard using React, Next.js, and Prisma.",
+    link: "https://status.aqayepardakht.ir",
+  },
+  {
+    id: 4,
+    name: "Aqaye Pardakht",
+    logo: "/images/companies/aqa.jpeg",
+    image: "/images/projects/AqaStatus.jpeg",
+    description:
+      "Built a full-stack dashboard using React, Next.js, and Prisma.",
+    link: "https://status.aqayepardakht.ir",
   },
 ];
